ca2dfa55ff1def3eb3ed82c5cae754cdf0168dcb
GuillaumeGETTE <guillaume.gette@gmail.com>
2017-05-22 17:21:44 +0200

    Updating signatures

--- Kernel.mli
+++ Kernel.mli
@@ -7,6 +7,7 @@ left : SYNTAX.formula list ;
 right : SYNTAX.formula list}
 type theorem
 exception Invalid
+val _rev : 'a list -> 'a list -> 'a list
 val conclusion : theorem -> sequent
 val sel_left : theorem -> pos:int -> principal:SYNTAX.formula -> theorem (*done*)
 val sel_right : theorem -> pos:int -> principal:SYNTAX.formula -> theorem (*done*)
@@ -15,14 +16,18 @@ val init_left : sequent -> principal:SYNTAX.formula -> theorem
 val init_right : sequent -> principal:SYNTAX.formula -> theorem
 
 val and_left : theorem -> theorem * SYNTAX.formula (*done*)
-val and_right : theorem -> theorem * SYNTAX.formula (*done*)
+val and_right : theorem -> theorem -> theorem * SYNTAX.formula (*done*)
 
 val true_left : theorem -> theorem * SYNTAX.formula (*done*)
-val true_right : sequent -> theorem
+val true_right : sequent -> theorem * SYNTAX.formula
 
-val false_left : sequent -> theorem
+val false_left : sequent -> theorem * SYNTAX.formula
 val false_right : sequent -> theorem * SYNTAX.formula (*done*)
 
-val or_left : theorem -> theorem * SYNTAX.formula (*done*)
+val or_left : theorem -> theorem -> theorem * SYNTAX.formula (*done*)
 val or_right : theorem -> theorem * SYNTAX.formula (*done*)
+
+val implies_left : theorem -> theorem -> theorem * SYNTAX.formula
+val implies_right : theorem -> theorem * SYNTAX.formula
+
 end
---
+++ ProofSearch.mli
@@ -0,0 +1,8 @@
+open Kernel.KERNEL
+
+module PROOFSEARCH = sig
+
+val search KERNEL.sequent -> int -> KERNEL.theorem
+
+
+end
